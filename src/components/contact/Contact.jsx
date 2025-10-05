84298e90f1409c966ed37f45d43ab8c0b1e458e4
vandita <vanditaj008@gmail.com>
2025-10-05 15:27:22 +0530

    email

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -70,7 +70,7 @@ export default function Contact() {
                   <path d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                 </svg>
                 <div className="ml-4 text-md font-semibold">
-                  contact@nextgenvectora.com
+                  anusha.joshi@nextgendatasets.com 
                 </div>
               </div>
             </div>
