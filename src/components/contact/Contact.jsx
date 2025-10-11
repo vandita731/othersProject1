9ea553cce9495c7f98f10a2f6d8390e2b21d0f0a
vandita <vanditaj008@gmail.com>
2025-10-12 03:35:45 +0530

    updates

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -53,26 +53,9 @@ export default function Contact() {
                 >
                   <path d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11 11 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                 </svg>
-                <div className="ml-4 text-md font-semibold">+91 97434 92247</div>
+                <div className="ml-4 text-md font-semibold">+91 9110205495</div>
               </div>
 
-              {/* Email */}
-              <div className="flex items-center mt-5 text-gray-700">
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="1.5"
-                  viewBox="0 0 24 24"
-                  className="w-8 h-8 text-[#d81b60]"
-                >
-                  <path d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-                </svg>
-                <div className="ml-4 text-md font-semibold">
-                  anusha.joshi@nextgendatasets.com 
-                </div>
-              </div>
             </div>
 
             {/* Contact Form */}
